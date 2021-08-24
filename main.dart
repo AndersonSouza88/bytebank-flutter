f8c73ecf7c77b6a68887de7c0d10cfa26a929980
Anderson Souza <anderson_razor_88@hotmail.com>
2021-08-24 20:16:39 -0300

    começando o app Bytebank

--- main.dart
+++ main.dart
@@ -1,48 +1,14 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
-
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  static const String _title = 'Bytebank';
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: _title,
-      home: MyStatefulWidget(),
-    );
-  }
-}
-
-
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({Key? key}) : super(key: key);
-
-  @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
-}
-
-
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  int _count = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Tranferências'),
-       backgroundColor: Colors.green,
-
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold (
+        appBar: AppBar (
+          title: Text ("Transfências"),
+        ),
       ),
+    ),
 
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => setState(() => _count++),
-        tooltip: 'Increment Counter',
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
+  );
 }
