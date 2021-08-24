08d68d2d82e2f9fadb777e3450b45065eaa3e0b5
Anderson Souza <anderson_razor_88@hotmail.com>
2021-08-24 18:41:11 -0300

    primeiros passos com o app Bytebank

--- main.dart
+++ main.dart
@@ -1,30 +1,46 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    Column(
-      children:  <Widget>[
-        Text(
-          'Deliver features faster',
-          textDirection: TextDirection.ltr,
-        ),
-        Text(
-          'Craft beautiful UIs',
-          textDirection: TextDirection.ltr,
-        ),
-        Text(
-          'Craft beautiful UIs',
-          textDirection: TextDirection.ltr,
-        ),
-        Column(
-          children:  <Widget>[
-            Text(
-              'Craft beautiful UIs',
-              textDirection: TextDirection.ltr,
-            ),
-          ],
-        ),
-      ],
-    ),
-  );
+void main() => runApp(const MyApp());
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  static const String _title = 'Bytebank';
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: _title,
+      home: MyStatefulWidget(),
+    );
+  }
+}
+
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  int _count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Tranferências'),
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => setState(() => _count++),
+        tooltip: 'Increment Counter',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
 }
