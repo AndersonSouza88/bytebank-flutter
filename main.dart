3af2b2b403138d5df55fa7c4fde379be90501ecb
Anderson Souza <anderson_razor_88@hotmail.com>
2021-08-24 21:45:04 -0300

    Primeira tela do APP

--- main.dart
+++ main.dart
@@ -3,12 +3,33 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold (
-        appBar: AppBar (
-          title: Text ("Transfências"),
+      home: Scaffold(
+        body: Column(
+          children: [
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.monetization_on),
+                title: Text ("100.00"),
+                subtitle: Text ("1000"),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.monetization_on),
+                title: Text ("200.00"),
+                subtitle: Text ("1000"),
+              ),
+            ),
+          ],
+        ),
+        appBar: AppBar(
+          title: Text("Transfências"),
+        ),
+        floatingActionButton: FloatingActionButton (
+          onPressed: () {},
+          child: Icon(Icons.add),
         ),
       ),
     ),
-
   );
 }
