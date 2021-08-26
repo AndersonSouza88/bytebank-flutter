6ca76233712e303895e442cae284ec105cdfcbb7
Anderson Souza <anderson_razor_88@hotmail.com>
2021-08-25 21:10:17 -0300

    codigo refatorado da primeira tela

--- main.dart
+++ main.dart
@@ -1,35 +1,63 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Column(
-          children: [
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.monetization_on),
-                title: Text ("100.00"),
-                subtitle: Text ("1000"),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.monetization_on),
-                title: Text ("200.00"),
-                subtitle: Text ("1000"),
-              ),
-            ),
-          ],
-        ),
+        body: ListaDeTransferencia(),
         appBar: AppBar(
           title: Text("Transfências"),
         ),
-        floatingActionButton: FloatingActionButton (
+        floatingActionButton: FloatingActionButton(
           onPressed: () {},
           child: Icon(Icons.add),
         ),
       ),
     ),
   );
+}
+
+class ListaDeTransferencia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ItemTransferencia(Transferencia(1000, 1001)),
+        ItemTransferencia(Transferencia(2000, 2002)),
+        ItemTransferencia(Transferencia(3000, 3003)),
+      ],
+    );
+  }
+}
+
+class Transferencia {
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
+}
+
+class ItemTransferencia extends StatelessWidget {
+  final Transferencia _transferencia;
+
+
+  ItemTransferencia(this._transferencia);
+
+  /*final String valor;
+  final String numeroConta;
+
+  ItemTransferencia(this.valor, this.numeroConta);*/
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.numeroConta.toString()),
+      ),
+    );
+  }
 }
