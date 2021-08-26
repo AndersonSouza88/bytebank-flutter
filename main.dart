bd85b70c42e326e1cb463ee0445488bb132d5d7b
Anderson Souza <anderson_razor_88@hotmail.com>
2021-08-25 21:53:10 -0300

    Começando segunda tela : Criando Transferência

--- main.dart
+++ main.dart
@@ -1,44 +1,66 @@
-import 'dart:ffi';
 
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: ListaDeTransferencia(),
-        appBar: AppBar(
-          title: Text("Transfências"),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Icon(Icons.add),
-        ),
+  runApp(Bytebank());
+}
+
+class Bytebank extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+          body:FormularioTransferencia(),
+        )
+    );
+  }
+}
+
+
+class FormularioTransferencia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Criando Transferência'),
       ),
-    ),
-  );
+      body: Text('Teste '),
+    );
+  }
 }
 
 class ListaDeTransferencia extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        ItemTransferencia(Transferencia(1000, 1001)),
-        ItemTransferencia(Transferencia(2000, 2002)),
-        ItemTransferencia(Transferencia(3000, 3003)),
-      ],
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Transfências"),
+        ),
+        body: Column(
+          children: [
+            ItemTransferencia(Transferencia(1000, 1001)),
+            ItemTransferencia(Transferencia(2000, 2002)),
+            ItemTransferencia(Transferencia(3000, 3003)),
+          ],
+        ),
+
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.add),
+        ),
+      ),
     );
   }
 }
-
 class Transferencia {
   final double valor;
   final int numeroConta;
 
   Transferencia(this.valor, this.numeroConta);
 }
-
 class ItemTransferencia extends StatelessWidget {
   final Transferencia _transferencia;
 
